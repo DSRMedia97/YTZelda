0ca468b977f4db4e972b2769fb204b85ef9fac20
Chris Eyrich <chriseyrich@gmail.com>
2022-06-23 20:35:38 -0400

    Adds experience to player on enemy death.

--- code/level.py
+++ code/level.py
@@ -79,7 +79,8 @@ class Level:
                                         [self.visible_sprites, self.attackable_sprites], 
                                         self.obstacle_sprites,
                                         self.damage_player, 
-                                        self.trigger_death_particles)
+                                        self.trigger_death_particles, 
+                                        self.add_exp)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
@@ -122,6 +123,9 @@ class Level:
 
     def trigger_death_particles(self, position, particle_type):
         self.animation_player.create_particles(particle_type, position, self.visible_sprites)
+
+    def add_exp(self, amount):
+        self.player.exp += amount
 
     def run(self):
         # update and draw the game
